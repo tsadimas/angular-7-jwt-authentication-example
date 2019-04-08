6920fb94f9156edbd6479b36966ef561e2cd1331
tsadimas <tsadimas@hua.gr>
2019-04-08 14:45:44 +0300

    changed for django jwt login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { LoginComponent } from './login';
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        fakeBackendProvider
+        //        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
